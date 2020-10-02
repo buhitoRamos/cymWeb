4e3773dfa637d2de57255a6b039af4081f89a3e6
Martin Lopez <martin.lopezx@neoris.com>
2020-10-01 22:46:06 -0300

    heredando el estado de login a formlogin

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -4,107 +4,17 @@ import $ from "jquery"
 
 class FormLogin extends React.Component {
 
-    state = {
-        data: {
-            user: "",
-            pass: ""
-        },
-        recived: {
-            user: "",
-            pass: "",
-            aproved: false
-        },
-        aux: {
-            chk: "password",
-            toInto: "false",
-            url:"#"
-        }
-    }
-
-    handleSubmit = e => {
-        this.handleClick()
-        e.preventDefault();
-    }
-
-    handleChangeChk = () => {
-        var myCheck = this.state.aux.chk
-        console.log(myCheck)
-        if (myCheck == "password") {
-            myCheck = "text"
-        } else {
-            myCheck = "password"
-        }
-        this.setState({
-            aux: {
-                chk: myCheck
-            }
-        })
-    }
-
-    setStateRecived = (user, pass) => {
-        this.setState({
-            recived: {
-                user: user,
-                pass: pass
-            }
-        })
-        if ((this.state.data.user + this.state.data.pass) ==
-            (this.state.recived.user + this.state.recived.pass)
-        ) {
-            console.log("es igual")
-            this.setState({
-                aux: {
-                    toInto: true,
-                }
-            })
-            console.log(this.state.aux.toInto)
-        } else {
-            console.log("datos que tengo: " + this.state.data.user + " " + this.state.data.pass)
-            console.log("datos que llegan: " + this.state.recived.user + " " + this.state.recived.pass)
-            console.log("es diferente")
-        }
-    }
-
-    handleChange = e => {
-        this.setState({
-            data: {
-                ...this.state.data,
-                [e.target.id]: e.target.value,
-            }
-        })
-    }
-
-
-
-    handleClick = () => {
-        $.ajax({
-            url: "http://localhost/backend/Login.php",
-            data: { us: this.state.data.user, pa: this.state.data.pass },
-            dataType: 'json',
-            type: 'POST',
-            async: true,
-            success: (response) => {
-                try {
-                    var { user, pass } = ""
-                    user = response[0].user
-                    pass = response[0].pass
-                    this.setStateRecived(user, pass)
-                } catch {
-                    user = ""
-                    pass = ""
-                }
-            }
-        })
-    }
+   
 
     render() {
+        const {handleChange, handleChangeChk, handleSubmit, handleClick, estado} = this.props
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <div className="container form-login">
                     <div>
                         <input
                             id="user"
-                            onChange={this.handleChange}
+                            onChange={handleChange}
                             placeholder="Ingrese Usuario"
                             type="text"
                             className="form-control form-input"
@@ -113,16 +23,16 @@ class FormLogin extends React.Component {
                     <div className="input-group-prepend">
                         <input
                             id="pass"
-                            onChange={this.handleChange}
+                            onChange={handleChange}
                             placeholder="Ingrese Password"
-                            type={this.state.aux.chk}
+                            type={estado.aux.chk}
                             className="form-control form-input"
                         />
                         <input
                             type="checkbox"
                             aria-label="Checkbox for following text input"
                             className="form-input-ch"
-                            onChange={this.handleChangeChk}
+                            onChange={handleChangeChk}
                         />
                     </div>
                     <div className="form-btn float-center">
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,8 +7,104 @@ import $ from "jquery"
 class Login extends React.Component {
     
     state = {
-        toInto:false
-    }  
+        data: {
+            user: "",
+            pass: ""
+        },
+        recived: {
+            user: "",
+            pass: "",
+            aproved: false
+        },
+        aux: {
+            chk: "password",
+            toInto: "false",
+            url:""
+        }
+    }
+
+    handleSubmit = e => {
+        this.handleClick()
+        e.preventDefault();
+    }
+
+    handleChangeChk = () => {
+        var myCheck = this.state.aux.chk
+        console.log(myCheck)
+        if (myCheck == "password") {
+            myCheck = "text"
+        } else {
+            myCheck = "password"
+        }
+        this.setState({
+            aux: {
+                chk: myCheck
+            }
+        })
+    }
+
+    setStateRecived = (user, pass) => {
+        this.setState({
+            recived: {
+                user: user,
+                pass: pass
+            }
+        })
+        if ((this.state.data.user + this.state.data.pass) ==
+            (this.state.recived.user + this.state.recived.pass)
+        ) {
+            console.log("es igual")
+            this.setState({
+                aux: {
+                    toInto: true,
+                    url: ""
+                }
+            })
+            console.log("toInto: "+this.state.aux.toInto)
+            
+            
+        } else {
+            console.log("datos que tengo: " + this.state.data.user + " " + this.state.data.pass)
+            console.log("datos que llegan: " + this.state.recived.user + " " + this.state.recived.pass)
+            console.log("es diferente")
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            data: {
+                ...this.state.data,
+                [e.target.id]: e.target.value,
+            }
+        })
+    }
+
+
+
+    handleClick = () => {
+        $.ajax({
+            url: "http://localhost/backend/Login.php",
+            data: { us: this.state.data.user, pa: this.state.data.pass },
+            dataType: 'json',
+            type: 'POST',
+            async: true,
+            success: (response) => {
+                try {
+                    var { user, pass } = ""
+                    user = response[0].user
+                    pass = response[0].pass
+                    this.setStateRecived(user, pass)
+                } catch {
+                    user = ""
+                    pass = ""
+                }
+            }
+        })
+    }
+
+
+
+
 
     render() {
         return (
@@ -19,7 +115,12 @@ class Login extends React.Component {
                 <div className="float-left">
                     <Logo />
                     <FormLogin
-                     />
+                    handleChange={this.handleChange}
+                    handleChangeChk={this.handleChangeChk}
+                    handleClick={this.handleClick}
+                    handleSubmit={this.handleSubmit}
+                    estado={this.state}
+                     />                    
 
                 </div>
             </div>
