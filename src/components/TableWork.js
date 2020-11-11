006f6a963a1575487175bcc684931c482ed9334e
Martin Lopez <martin.lopezx@neoris.com>
2020-11-11 15:28:29 -0300

    primer commit - comenzando a modificar funcionamiento de customerTable

--- src/components/TableWork.js
+++ src/components/TableWork.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import "../components/styles/Tbody.css";
 
-function TableWork(props) {   
-   
+function TableWork(props) {      
 
     return (
         <div className="Work">
